50a1ecc46cdede68659fab44107682bd68a7eaba
Elizabeth Gonzalez <maximilianoruizgonzalez@MacBook-de-Maximiliano.local>
2022-11-23 20:14:38 -0600

    2

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ const { response } = require("express");
 var bodyParser = require('body-parser')
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, './client/build')));
+//app.use(express.static(path.resolve(__dirname, './client/build')));
 //app.use(express.static(path.join(__dirname, 'build')));
-
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extendeded: true}))
 app.use(bodyParser.json())
 
