3e428c2bc0e9831c655ddb4baeda724eab1f8b36
Elizabeth Gonzalez <maximilianoruizgonzalez@MacBook-de-Maximiliano.local>
2022-11-24 06:52:54 -0600

    3

--- index.js
+++ index.js
@@ -92,6 +92,11 @@ app.post('/invoice', (req, res) => {
   }
 })
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
